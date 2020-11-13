68f9c5682598ec91b8d409c8cb55db0cd8fcdcda
meghats123 <meghats39032@gmail.com>
2020-11-13 22:37:25 +0530

    screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main()=>runApp(MyApp());
-class MyApp extends StatelessWidget {
-  TextEditingController getFirstnumber=TextEditingController();
-  TextEditingController getSecondnumber=TextEditingController();
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController getFirstnum=TextEditingController();
+  TextEditingController getSecondnum=TextEditingController();
+  int result=0;
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +21,7 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Calculator App"),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.amber,
         ),
         body: Container(
           padding: EdgeInsets.all(10.0),
@@ -22,7 +29,7 @@ class MyApp extends StatelessWidget {
             child: Column(
               children: [
                 TextField(
-                  controller: getFirstnumber,
+                  controller: getFirstnum,
                   decoration: InputDecoration(
                     hintText: "Enter the first number",
                     border: OutlineInputBorder()
@@ -30,7 +37,7 @@ class MyApp extends StatelessWidget {
                 ),
                 SizedBox(height: 10.0,),
                 TextField(
-                  controller: getSecondnumber,
+                  controller: getSecondnum,
                   decoration: InputDecoration(
                     hintText: "Enter the second number",
                     border: OutlineInputBorder()
@@ -38,46 +45,61 @@ class MyApp extends StatelessWidget {
                 ),
                 SizedBox(height: 10.0,),
                 RaisedButton(onPressed: (){
-                  int getnum1=int.parse(getFirstnumber.text);
-                  int getnum2=int.parse(getSecondnumber.text);
-                  int result=getnum1+getnum2;
+                  int getnum1=int.parse(getFirstnum.text);
+                  int getnum2=int.parse(getSecondnum.text);
+
+                  setState(() {
+                     result=getnum1+getnum2;
+
+                  });
                   print(result);
+
                 },
-                  color: Colors.red,
+                  color: Colors.amber,
                   child: Text("ADD"),
                 ),
                 SizedBox(height: 10.0,),
                 RaisedButton(onPressed: (){
-                  int getnum1=int.parse(getFirstnumber.text);
-                  int getnum2=int.parse(getSecondnumber.text);
-                  int result=getnum1-getnum2;
+                  int getnum1=int.parse(getFirstnum.text);
+                  int getnum2=int.parse(getSecondnum.text);
+                  setState(() {
+                     result=getnum1-getnum2;
+                  });
                   print(result);
+
                 },
-                  color: Colors.red,
+                  color: Colors.amber,
                   child: Text("SUB"),
                 ),
                 SizedBox(height: 10.0,),
                 RaisedButton(onPressed: (){
-                  int getnum1=int.parse(getFirstnumber.text);
-                  int getnum2=int.parse(getSecondnumber.text);
-                  int result=getnum1*getnum2;
+                  int getnum1=int.parse(getFirstnum.text);
+                  int getnum2=int.parse(getSecondnum.text);
+                  setState(() {
+                     result=getnum1*getnum2;
+                  });
                   print(result);
 
                 },
-                  color: Colors.red,
+                  color: Colors.amber,
                   child: Text("MUL"),
                 ),
                 SizedBox(height: 10.0,),
                 RaisedButton(onPressed: (){
-                  int getnum1=int.parse(getFirstnumber.text);
-                  int getnum2=int.parse(getSecondnumber.text);
-                  double result=getnum1/getnum2;
-                  print(result);
+                  int getnum1=int.parse(getFirstnum.text);
+                  int getnum2=int.parse(getSecondnum.text);
+                  setState(() {
+                    result=getnum1~/getnum2;
+                  });
+                   print(result);
 
                 },
-                  color: Colors.red,
+                  color: Colors.amber,
                   child: Text("DIV"),
-                )
+                ),
+                Text(result.toString(),
+                    style:TextStyle(fontSize: 20.0),
+                ),
               ],
             ),
           ),
@@ -86,3 +108,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
